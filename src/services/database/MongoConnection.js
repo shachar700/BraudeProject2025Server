98a0c611657b5030bfed986741802017cc063ad6
shachar700 <shachar700@gmail.com>
2025-11-02 14:15:33 +0200

    added MongoDBConnection

--- src/services/database/MongoConnection.js
+++ src/services/database/MongoConnection.js
@@ -1,3 +1,26 @@
-// TODO singleton mongoose connection, with url from .env
+// singleton mongoose connection, with uri from .env
 
-// ## check if require(...) type import is singleton
+// ## check if require(...) type import is singleton - yes it is
+
+const mongoose = require('mongoose');
+
+function connectToDB() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    console.error('❌ MONGO_URI not set in .env');
+    process.exit(1);
+  }
+
+  mongoose.connect(uri);
+
+  mongoose.connection.on('connected', () => {
+    console.log('✅ Connected to MongoDB');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error('❌ MongoDB connection error:', err);
+  });
+}
+
+module.exports = connectToDB;
